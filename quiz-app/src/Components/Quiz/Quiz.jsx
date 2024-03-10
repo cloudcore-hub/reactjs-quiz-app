d06bc640a938b054eef3b50af62458b11d9738cb
Ogo Ozotta <ogochukwu.ozotta@gmail.com>
2024-03-09 18:28:00 -0600

    adding meta env

--- quiz-app/src/Components/Quiz/Quiz.jsx
+++ quiz-app/src/Components/Quiz/Quiz.jsx
@@ -22,7 +22,8 @@ const Quiz = () => {
     const fetchQuestions = async () => {
       if (startQuiz) {
         try {
-          const apiUrl = import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:3000/api/questions';
+          // const apiUrl = import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:3000/api/questions';
+          const apiUrl = "http://quizapp.cloudcorehub.com/api/questions"
           const response = await fetch(apiUrl);
           if (!response.ok) throw new Error("Failed to fetch");
           let questions = await response.json();
